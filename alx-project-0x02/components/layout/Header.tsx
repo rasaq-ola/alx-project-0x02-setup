c46c1a7f7fa8dee78a4a42d63647a69699775e5d
Rasaq-ola <rasaqola196@gmail.com>
2025-06-04 17:55:32 +0100

    modifying Header.tsx to include Link navigation

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import Link from 'next/link';
+
 const Header = () => {
     return (
         <header className="bg-gray-800 text-white p-4">
@@ -5,9 +8,9 @@ const Header = () => {
             <h1 className="text-2xl font-bold">Placeholder</h1>
             <nav>
             <ul className="flex space-x-4">
-                <li><a href="/" className="hover:underline">Home</a></li>
-                <li><a href="/about" className="hover:underline">About</a></li>
-                <li><a href="/contact" className="hover:underline">Contact</a></li>
+                <li><Link href="/" className="hover:underline">Home</Link></li>
+                <li><Link href="/about" className="hover:underline">About</Link></li>
+                <li><Link href="/contact" className="hover:underline">Contact</Link></li>
             </ul>
             </nav>
         </div>
