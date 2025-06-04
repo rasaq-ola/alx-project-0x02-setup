01db93d55af9b85a5e7404fd90b8f23dd70ba201
Rasaq-ola <rasaqola196@gmail.com>
2025-06-04 17:37:48 +0100

    implementing basic routing using the pages router

---
+++ alx-project-0x02/pages/about.tsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>This is the about page of our application.</p>
+    </div>
+  );
+}
+export default About;
+// This code defines a functional component named About that returns a JSX element.
