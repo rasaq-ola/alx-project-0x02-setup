553f2bcf8631f6608d14945eaa1cdbe21dacd2d2
Rasaq-ola <rasaqola196@gmail.com>
2025-06-05 03:02:22 +0100

    fix: use getStaticProps as named function

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -23,7 +23,7 @@ const Users: React.FC<UsersPageProps> = ({ users }) => {
   );
 };
 
-export const getStaticProps = async () => {
+export async function getStaticProps() {
   const res = await fetch('https://jsonplaceholder.typicode.com/users');
   const data = await res.json();
 
